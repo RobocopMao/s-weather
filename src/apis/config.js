23d46ba2b5cab075813bb054b2499c8530bef07d
RobocopMao <410503915@qq.com>
2019-08-06 21:42:10 +0800

    config，和风天气的签名参数一直没弄好，按文档步骤，还是签名无效，暂时不用签名了

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,27 +1,59 @@
 import Taro from '@tarojs/taro'
 import moment from 'moment'
 import HmacSHA1 from 'crypto-js/hmac-sha1'
+import MD5 from 'crypto-js/md5'
 import Base64 from 'crypto-js/enc-base64'
-
-// export const HOST = 'https://www.mxnzp.com';
-export const HOST = 'https://api.seniverse.com';
-
-// export const HFKEY = '';  // 和风天气的key
-// export const CONDICONHOST = 'https://cdn.heweather.com';
+import Utf8 from 'crypto-js/enc-utf8'
+import forEach from 'lodash/each'
 
 // 腾讯地图key
-export const QQ_MAP_SDK_KEY = '';
+export const QQ_MAP_SDK_KEY = 'WATBZ-IBNR4-4VHU2-D2LSO-PBQBQ-G3BKJ';
 
-// 生成签名参数
-export const createSignParams = () => {
-  const PUBLIC_KEY = '';
-  const PRIVATE_KEY = '';
+// 和风天气接口标识
+export const HF_HOST_TYPE = 'HF';
+// 心知天气接口标识
+export const XZ_HOST_TYPE = 'XZ';
+
+// 和风天气接口请求地址
+export const HF_API_HOST_FREE = 'https://free-api.heweather.net'; // 天气等
+export const HF_API_HOST_SEARCH = 'https://search.heweather.net'; // 热门城市,城市搜索专用
+// 心知天气接口请求地址
+export const XZ_API_HOST = 'https://api.seniverse.com';
+
+// 签名算法
+export const createSignParams = (hostType, params) => {
   const ts = moment().unix();
-  const ttl = 600;
-  const signStr = `ts=${ts}&ttl=${ttl}&uid=${PUBLIC_KEY}`;
-  const hmacSHA1 = HmacSHA1(signStr, PRIVATE_KEY).toString(Base64);
-  // console.log("hmacSHA1 = %s", hmacSHA1);
-  const sig = encodeURI(hmacSHA1);
-  // console.log(sig);
-  return {ts, ttl, uid: PUBLIC_KEY, sig}
+  if (hostType === 'XZ') { // 心知天气生成签名参数 HMAC-SHA1
+    const XZ_API_KEY_PUBLIC = '';
+    const XZ_API_KEY_PRIVATE = '';
+    const ttl = 600;
+    const signStr = `ts=${ts}&ttl=${ttl}&uid=${XZ_API_KEY_PUBLIC}`;
+    const hmacSHA1 = HmacSHA1(signStr, XZ_API_KEY_PRIVATE).toString(Base64);
+    // console.log("hmacSHA1 = %s", hmacSHA1);
+    const sig = encodeURI(hmacSHA1);
+    // console.log(sig);
+    return {ts, ttl, uid: XZ_API_KEY_PUBLIC, sig}
+  } else { // 和风天气生成签名参数 MD5
+    const HF_API_KEY = '';
+    // const HF_USERNAME = '';
+    // let paramsStr = '';
+    // forEach(params, (v, k) => {
+    //   if (v !== '') {
+    //     paramsStr += `&${k}=${v}`;
+    //   }
+    // });
+    // const signStr = `${paramsStr}${HF_API_KEY}`.slice(1);
+    // // const signStr = `t=${ts}&username=${HF_USERNAME}${HF_API_KEY}`;
+    // console.log(signStr);
+    // const signUtf8 = MD5(signStr).toString();
+    // // console.log(MD5(signStr));
+    // console.log(signUtf8);
+    // const signBase64 = Utf8.parse(signUtf8).toString(Base64);
+    // console.log(signBase64);
+    // const sign = signBase64;
+    // console.log(sign);
+    // return {username: HF_USERNAME, t: ts, sign};
+    return {key: HF_API_KEY};
+  }
+
 };
